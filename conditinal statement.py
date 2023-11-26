fead479a1e0bc87053712d5de6b9a088ecd0fbc2
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-26 12:34:26 +0500

    Add files via upload

---
+++ conditinal statement.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 12:24:33 2023
+
+@author: Web 123
+"""
+
+# x=5
+# y=6
+# print('x=',x,'y=',y)
+# print('checking less then with\'<\':',x<y)
+# print('checking less then with\'>\' ',x>y)
+
+# less then and equality not equal equal less then equal greaterthen equal
+
+
+# var_1=int(input('Enter First Number>>'))
+# var_2=int(input('Enter Second Number>>'))
+# print('variable 1',var_1,'variable 2',var_2)
+# print('checking equality wit \'==\'''>>',var_1 == var_2)
+# print('checking not equal with\'!=\'''>>',var_1 != var_2)
+# print('checking lessthen or equal\'\<=''>>',var_1 <= var_2)
+# print('checking greaterthen or equal\'\>=''>>',var_1 >= var_2)
+
+# print('demostrate that \'True\'is boolean in python \'\'type(True)'':',type(True))
